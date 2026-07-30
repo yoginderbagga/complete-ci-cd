4def77fb4a7042258679b410167116adf9c736c2
Yoginder Bagga <yoginder.bagga@gmail.com>
2026-07-30 17:32:49 +0530

    Added a change to main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker CI/CD Pipeline"
+    return "Hello from Docker CI/CD Pipeline + Now this is second Update"
 
 app.run(host="0.0.0.0", port=8080)
